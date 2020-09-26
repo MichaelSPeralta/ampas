bcd6268834572c20c7707a33adb8e5a1ab1255aa
MichaelSPeralta <53713516+MichaelSPeralta@users.noreply.github.com>
2020-09-26 17:11:25 -0300

    Comienzo section 1

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function menuFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+// function menuFunction() {
+//   var x = document.getElementById("myLinks");
+//   var a = document.getElementById("myHeader");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//     a.style.boxShadow = "0px 3px 11px rgba(0, 0, 0, 0.25)";
+//   } else {
+//     x.style.display = "block";
+//     a.style.boxShadow = "none";
+//   }
+// }
