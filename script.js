138c657ce577d2fff8fee07c47079f720e3e57cf
MichaelSPeralta <53713516+MichaelSPeralta@users.noreply.github.com>
2020-09-27 17:05:32 -0300

    agrego modelos de media queries css, comienzo js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-// function menuFunction() {
-//   var x = document.getElementById("myLinks");
-//   var a = document.getElementById("myHeader");
-//   if (x.style.display === "block") {
-//     x.style.display = "none";
-//     a.style.boxShadow = "0px 3px 11px rgba(0, 0, 0, 0.25)";
-//   } else {
-//     x.style.display = "block";
-//     a.style.boxShadow = "none";
-//   }
-// }
+function menuFunction() {
+  var x = document.getElementById("myLinks");
+  var a = document.getElementById("myHeader");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    a.style.boxShadow = "0px 3px 11px rgba(0, 0, 0, 0.25)";
+    a.style.height = "40px";
+  } else {
+    x.style.display = "block";
+    a.style.boxShadow = "none";
+    a.style.height = "auto";
+  }
+}
