9640a7d7f2c0ea96fc7915e00ffac32fe72404f9
EmanuelHernandez <56940585+EmanuelHernandez@users.noreply.github.com>
2020-10-13 18:15:00 -0300

    Update action.php
    Actualizacion desde host 000webhost

--- action.php
+++ action.php
@@ -1,35 +1,44 @@
 
 <?php
 
+// Variables de conexion a BD.
 $servername = "localhost";
-$database = "amseguros";
-$username = "root";
-$password = "";
+$username = "id14981681_db_test";
+$password = "\$mS3gur0s1258";
+$database = "id14981681_test";
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
+
 // Check connection
 if (!$conn) {
     die("Error con la conexion del servidor: " . mysqli_connect_error());
+    echo "No se puede conectar";
 }
-echo "Connected successfully";
-
+// echo "Connected successfully";
 
 //Recibimos por POST datos procedentes del formulario index
-if (isset($_POST["nom"])){
-
-    $nombre = $_POST["nom"];
-    $apellido = $_POST["ape"];
-    $numero = $_POST["num"];
-    $email = $_POST["mail"];
-    $seguro = $_POST["seg"];
+$nombre = $_POST["nom"];
+$apellido = $_POST["ape"];
+$numero = $_POST["num"];
+$email = $_POST["mail"];
+$seguro = $_POST["seg"];
 
 //Consulta SQL
-$agregarReg = "INSERT INTO formulario (Nombre,Apellido,Numero,Email,Tipo_de_Seguro) values ('$nombre','$apellido','$numero','$email','$seguro')";
-//Ejecutamos query 
-$ejecutarQuery = mysqli_query($conn,$agregarReg);
-//Redireccionamos a menu principal
-echo  $_POST['nom'];
-header("location:index.html");
+$agregarReg = "INSERT INTO formulario (nombre,apellido,numero,email,seguro) VALUES ('$nombre','$apellido','$numero','$email','$seguro')";
+
+//Comprobamos datos enviadosa la bd
+if ($_POST["registrar"]){
+    if (!$conn) {
+    die("Error con la conexion del servidor: " . mysqli_connect_error());
+    echo "No se puede conectar";
+}else {
+    $ejecutarQuery = mysqli_query($conn,$agregarReg);
+    echo  "$nombre lo estaremos contactando a la brevedad.";
+}
+
     }
+    
+// header("location:index.html");
 //Cerramos conexion a la base de datos.
 mysqli_close($conn);
